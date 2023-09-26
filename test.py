20a4e40c87e3fa9ed45d8ea9954452223cfbd988
IuliiaKichigina <118169444+IuliiaKichigina@users.noreply.github.com>
2023-09-26 18:37:46 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-# Юлия Кичигина, 8а когорта - Финальный проект. Инженер по тестированию плюс
-import sender_stand_request
-
-
-# 4.Проверить, что код ответа равен 200.
-def test_receive_info():
-    info_response = sender_stand_request.get_info()
-    assert info_response.status_code == 200
