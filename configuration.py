7077c397a9f7e63eb55b5b5b77eff98671d63997
IuliiaKichigina <118169444+IuliiaKichigina@users.noreply.github.com>
2023-09-26 18:39:36 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_PATH = "/api/v1/orders/track"
+URL_SERVICE = "https://5fb6ed8d-d7b4-4f6e-905a-6012eee2475d.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
